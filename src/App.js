d0fe101089942c6312c0add1917d06a8a0f24983
nicknamejv <jonvercabral@yahoo.com>
2022-03-02 15:07:25 -0800

    completed navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Cover from './components/Cover/cover';
 import Body from './components/Body/body';
+import NavBar from './components/Navbar/navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Cover />
       <Body />
     </div>
